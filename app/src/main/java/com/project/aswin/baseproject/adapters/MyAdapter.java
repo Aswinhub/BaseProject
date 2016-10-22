4b275af9ca90462d92186928d376684953d3666a
Aswin <aswinkumar@tesark.in>
2016-10-22 15:39:30 +0530

    initial commit

---
+++ app/src/main/java/com/project/aswin/baseproject/adapters/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.project.aswin.baseproject.adapters;
+
+/**
+ * Created by Aswin on 10/22/2016.
+ */
+
+public class MyAdapter {
+}
---
+++ app/src/main/java/com/project/aswin/baseproject/model/LoginResponse.java
@@ -0,0 +1,10 @@
+package com.project.aswin.baseproject.model;
+
+import java.io.Serializable;
+
+/**
+ * Created by Aswin on 10/22/2016.
+ */
+
+public class LoginResponse implements Serializable {
+}
